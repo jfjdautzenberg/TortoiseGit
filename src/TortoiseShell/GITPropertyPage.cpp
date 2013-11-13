346a4076f9b4834e4c4229eba70f482b79f8ca5e
Frank Dautzenberg <frank.dautzenberg@nspyre.nl>
2013-11-13 13:53:07 +0100

    experiment with 2 changed files

--- src/TortoiseShell/GITPropertyPage.cpp
+++ src/TortoiseShell/GITPropertyPage.cpp
@@ -13,6 +13,8 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
+// modified by JFJD
+
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software Foundation,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
